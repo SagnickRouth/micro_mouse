b0430d904695830d32fe55b3478bf7f173d65cc4
Sagnick Routh <92044881+SagnickRouth@users.noreply.github.com>
2026-09-18 12:41:43 +0530

    Expose OLED hardware test screen

--- include/oled.h
+++ include/oled.h
@@ -12,5 +12,9 @@ void oled_init(void);
 void oled_clear(void);
 void oled_show_algorithm(const char *name, bool running);
 void oled_show_message(const char *line1, const char *line2);
+void oled_show_hardware_test(bool button_pressed, uint8_t dip,
+                             const char *algorithm,
+                             const char *left_count,
+                             const char *right_count);
 
 #endif
